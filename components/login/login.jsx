6bbe3525e90401a8155d881c0d23a43f8d41f040
abdulhamid1606 <abdulhamidturgunov1606@gmail.com>
2024-02-07 17:07:27 +0500

    Start

---
+++ components/login/login.jsx
@@ -0,0 +1,24 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <Box w={'1350px'} m={'0 auto'}>
+        <Box color={'#fff'}>
+            <Box> lazizbek rahmiddinov</Box>
+            <Box>
+                 
+            </Box>
+        </Box>
+        <Box>
+
+        </Box>
+        <Box>
+            <Box></Box>
+            <Box></Box>
+        </Box>
+    </Box>
+)
+}
+
+export default Login
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Box, Heading } from '@chakra-ui/react'
+import './App.css'
+import Login from '../components/login/login'
+
+function App() {
+
+  return (
+   <Box bg={'#1E1E1E'} w={'100%'} h={'100vh'}>
+      <Login />
+   </Box>
+  )
+}
+
+export default App
